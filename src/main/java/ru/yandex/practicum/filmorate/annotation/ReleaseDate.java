137f75dbe9098c32507548e3197111a29d8784e4
AlexanderTsybulskiy <kaktus3333@yandex.ru>
2023-09-06 21:00:14 +0300

    fix: code refactoring #2

--- src/main/java/ru/yandex/practicum/filmorate/annotation/ReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/ReleaseDate.java
@@ -6,7 +6,7 @@ import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = ReleaseDateConstraintValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target({ ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ReleaseDate {
     String message() default "{ReleaseDate}";
